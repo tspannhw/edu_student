cdaa09f054d7d8eec894a213f2a6adcfc00ffbdf
Ubuntu <ubuntu@ip-172-30-0-171.us-west-1.compute.internal>
2020-11-18 23:03:24 +0000

    Added rest api to rebuild-ats-hbase to restart yarn.

--- sbin/yarn/rebuild-ats-hbase.sh
+++ sbin/yarn/rebuild-ats-hbase.sh
@@ -21,8 +21,14 @@
 # VARIABLE
 NUMARGS=$#
 DIR=${HOME}
+COUNTER=0
+#AMBARI_URL=http://admin01.cloudair.lan:8080
+AMBARI_URL=https://admin01.cloudair.lan:8443
+AMBARI_USER=admin
+AMBARI_PASSWORD=admin
 DATETIME=$(date +%Y%m%d%H%M)
-LOGFILE=${DIR}/log/checkpoint-hdfs.log
+LOGDIR=/var/log/ambari-server
+LOGFILE=${LOGDIR}/log/checkpoint-hdfs.log
 
 # FUNCTIONS
 function usage() {
@@ -43,25 +49,40 @@ function callInclude() {
 }
 
 function cleanATS() {
-# Clean up ATS
 	sudo -u yarn-ats kinit -kt /etc/security/keytabs/yarn-ats.hbase-client.headless.keytab "yarn-ats-cloudair@CLOUDAIR.LAN"
 
 	sudo -u yarn-ats yarn app -destroy ats-hbase
-	sudo -u yarn-ats hdfs dfs -rm -R ./3.1.5.0-152/*
+	sudo -u yarn-ats hdfs dfs -rm -r ./3.1.5.0-152/*
 }
 
 function cleanHDFS() {
 # Clean up HDFS 
 	sudo -u hdfs kinit -kt /etc/security/keytabs/hdfs.headless.keytab "hdfs-cloudair@CLOUDAIR.LAN"
 
-	sudo -u hdfs hdfs dfs -rm -R /services/sync/yarn-ats/hbase.yarnfile
+	sudo -u hdfs hdfs dfs -rm -r -skipTrash /services/sync/yarn-ats/hbase.yarnfile
 }
 
-function restartYARN() {
+function setClusterName() {
+# Set the name of the cluster to a variable
 
-	echo "Restart YARN"
+        CLUSTER_NAME=$(curl -k -u ${AMBARI_USER}:${AMBARI_PASSWORD} -H "X-Requested-By: ambari" -i ${AMBARI_URL}/api/v1/clusters | grep '"cluster_name" :' | awk '{ print $3 }' | sed 's/,//g' | sed 's/"//g')
 }
 
+function stopYARN() {
+# Start the cluster
+
+        echo "Ambari Server stopping YARN on ${CLUSTER_NAME} at $(date)" | tee -a ${LOGFILE}
+
+        /usr/bin/curl -k -s -u ${AMBARI_USER}:${AMBARI_PASSWORD} -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Stop YARN via REST"}, "Body": {"ServiceInfo": {"state": "INSTALLED"}}}' ${AMBARI_URL}/api/v1/clusters/${CLUSTER_NAME}/services/YARN
+}
+
+function startYARN() {
+# Start the cluster
+
+        echo "Ambari Server starting YARN on ${CLUSTER_NAME} at $(date)" | tee -a ${LOGFILE}
+
+        /usr/bin/curl -k -s -u ${AMBARI_USER}:${AMBARI_PASSWORD} -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start YARN via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' ${AMBARI_URL}/api/v1/clusters/${CLUSTER_NAME}/services/YARN
+}
 
 # MAIN
 # Source Functions
@@ -74,4 +95,7 @@ checkArg 0
 # Clean
 cleanATS
 cleanHDFS
-restartYARN
+setClusterName
+stopYARN
+sleep 30
+startYARN
